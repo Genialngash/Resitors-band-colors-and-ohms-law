e08348e3221b06aec0e8bd14dda2b8d067fc5cd8
Genialngash <ngangadanche@gmail.com>
2021-03-29 17:34:58 +0300

    - function that calculates the value for each field provided only two inputs are present -changed the type of keyboard -show error message incase on invalid inputs

--- lib/pages/Ohms_Law_calculator.dart
+++ lib/pages/Ohms_Law_calculator.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:resistohms/constants.dart';
@@ -9,7 +11,11 @@ class OhmsLawCalculator extends StatefulWidget {
 }
 
 class _OhmsLawCalculatorState extends State<OhmsLawCalculator> {
-  TextEditingController _controller = TextEditingController();
+  TextEditingController _voltageController = TextEditingController();
+  TextEditingController _resistanceController = TextEditingController();
+  TextEditingController _powerController = TextEditingController();
+  TextEditingController _currentController = TextEditingController();
+
   List<DropdownMenuItem<int>> voltDownMenuItems = [
     DropdownMenuItem(
       child: Text('MegaVolts'),
@@ -29,14 +35,22 @@ class _OhmsLawCalculatorState extends State<OhmsLawCalculator> {
     ),
   ];
 
-  int voltsValue = 1;
+  int voltsMultiplier = 1;
   var voltage;
   var current;
   var resistance;
   var power;
+  var inputVoltage;
+  String resistanceHintText = 'resistance';
+  String powerHintText = 'power';
 
   @override
   Widget build(BuildContext context) {
+    bool voltageFieldIsEmpty = _voltageController.text.isEmpty;
+    bool currentFieldIsEmpty = _currentController.text.isEmpty;
+    bool resistanceFieldIsEmpty = _resistanceController.text.isEmpty;
+    bool powerFieldIsEmpty = _powerController.text.isEmpty;
+
     return SingleChildScrollView(
       child: Container(
         alignment: Alignment.center,
@@ -48,61 +62,72 @@ class _OhmsLawCalculatorState extends State<OhmsLawCalculator> {
             ListTile(
               title: TextField(
                 cursorWidth: 3.5,
-                controller: _controller,
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: _voltageController,
                 maxLength: 14,
                 cursorColor: Color(0xff98DEDA),
                 style: textStyling(fontSize: 30),
-                textAlign: TextAlign.center,
+                textAlign: TextAlign.start,
                 decoration: InputDecoration(
                   border: UnderlineInputBorder(),
                   hintText: 'voltage',
                   counter: Offstage(),
                 ),
                 onChanged: (String input) {
-                  input = ;
-                  
-                  print(input);
-                  setState(() {});
+                  print(voltage);
+                  setState(() {
+                    inputVoltage = double.tryParse(input);
+                  });
                 },
-                onEditingComplete: () {
-                  _controller.clear();
-                },
+                // onEditingComplete: () {
+                //   _voltageController.clear();
+                // },
               ),
               trailing: DropdownButton(
                 dropdownColor: Color(0xffC0C3DC),
                 items: voltDownMenuItems,
                 underline: Offstage(),
-                onChanged: (int newValue) {
+                onChanged: (int value) {
+                  if (value == 99) {
+                    setState(() {
+                      voltsMultiplier =
+                          value; // change the value to its respective name of the prefix
+                    });
+                    voltage = inputVoltage * 0.001;
+                  } else {
+                    setState(() {
+                      voltsMultiplier =
+                          value; // change the value to its respective name of the prefix
+                    });
+                    voltage = inputVoltage * value;
+                  }
+                  print(voltage);
+                },
+                value: voltsMultiplier,
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                cursorWidth: 4.0,
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: _currentController,
+                maxLength: 14,
+                cursorColor: Color(0xff98DEDA),
+                style: textStyling(fontSize: 30),
+                textAlign: TextAlign.start,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: 'current',
+                  counter: Offstage(),
+                ),
+                onChanged: (String input) {
                   setState(() {
-                    voltsValue = newValue;
+                    current = double.tryParse(input);
                   });
                 },
-                value: voltsValue,
-              ),
-            ),
-            OhmsListTile(
-                controller: _controller, dropDownMenuItems: voltDownMenuItems),
-            ListTile(
-              title: TextField(
-                cursorWidth: 4.0,
-                controller: _controller,
-                maxLength: 14,
-                cursorColor: Color(0xff98DEDA),
-                style: textStyling(fontSize: 30),
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  hintText: 'resistance',
-                  counter: Offstage(),
-                ),
-                onChanged: (String input) {
-                  input = input.toUpperCase();
-                  print(input);
-                  setState(() {});
-                },
-                onEditingComplete: () {
-                  _controller.clear();
-                },
+                // onEditingComplete: () {
+                //   _voltageController.clear();
+                // },
               ),
               trailing:
                   DropdownButton(items: voltDownMenuItems, onChanged: null),
@@ -110,28 +135,114 @@ class _OhmsLawCalculatorState extends State<OhmsLawCalculator> {
             ListTile(
               title: TextField(
                 cursorWidth: 4.0,
-                controller: _controller,
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: _resistanceController,
                 maxLength: 14,
                 cursorColor: Color(0xff98DEDA),
                 style: textStyling(fontSize: 30),
-                textAlign: TextAlign.center,
+                textAlign: TextAlign.start,
                 decoration: InputDecoration(
                   border: UnderlineInputBorder(),
-                  hintText: 'power',
+                  hintText: resistanceHintText,
                   counter: Offstage(),
                 ),
                 onChanged: (String input) {
-                  input = input.toUpperCase();
-                  print(input);
-                  setState(() {});
+                  setState(() {
+                    resistance = double.tryParse(input);
+                  });
                 },
-                onEditingComplete: () {
-                  _controller.clear();
-                },
+                // onEditingComplete: () {
+                //   _voltageController.clear();
+                // },
               ),
               trailing:
                   DropdownButton(items: voltDownMenuItems, onChanged: null),
             ),
+            ListTile(
+              title: TextField(
+                cursorWidth: 4.0,
+                keyboardType: TextInputType.numberWithOptions(),
+                controller: _powerController,
+                maxLength: 14,
+                cursorColor: Color(0xff98DEDA),
+                style: textStyling(fontSize: 30),
+                textAlign: TextAlign.start,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: powerHintText,
+                  counter: Offstage(),
+                ),
+                onChanged: (String input) {
+                  setState(() {
+                    power = double.tryParse(input);
+                  });
+                },
+                // onEditingComplete: () {
+                //   _voltageController.clear();
+                // },
+              ),
+              trailing:
+                  DropdownButton(items: voltDownMenuItems, onChanged: null),
+            ),
+            RaisedButton(onPressed: () {
+              try {
+                //check if the current field is empty first
+                if (currentFieldIsEmpty) {
+                  if (powerFieldIsEmpty) {
+                    //calculate current from voltage and resistance only if power field is empty
+                    _currentController.text = (voltage / resistance).toString();
+                  } else if (resistanceFieldIsEmpty) {
+                    //calculate current from power and  voltage only
+                    _currentController.text = (power / voltage).toString();
+                  } else if (voltageFieldIsEmpty) {
+                    //calculate current from power and resistance only
+                    _currentController.text =
+                        (sqrt(power / resistance)).toString();
+                  } else {
+                    buildSnackbar(context, '  INPUT TWO FIELDS ');
+                  }
+                } else if (voltageFieldIsEmpty) {
+                  if (powerFieldIsEmpty) {
+                    _voltageController.text = (current * resistance).toString();
+                  } else if (resistanceFieldIsEmpty) {
+                    _voltageController.text = (power / current).toString();
+                  } else if (currentFieldIsEmpty) {
+                    _voltageController.text =
+                        (sqrt(power / resistance)).toString();
+                  } else {
+                    buildSnackbar(context, '  INPUT TWO FIELDS ');
+                  }
+                } else if (resistanceFieldIsEmpty) {
+                  if (voltageFieldIsEmpty) {
+                    _resistanceController.text =
+                        (power / (current * current)).toString();
+                  } else if (currentFieldIsEmpty) {
+                    _resistanceController.text =
+                        ((pow(voltage, 2)) / power).toString();
+                  } else if (powerFieldIsEmpty) {
+                    _resistanceController.text = (voltage / current).toString();
+                  } else {
+                    buildSnackbar(context, '  INPUT TWO FIELDS ');
+                  }
+                } else if (powerFieldIsEmpty) {
+                  if (currentFieldIsEmpty) {
+                    _powerController.text =
+                        ((pow(voltage, 2)) / resistance).toString();
+                  } else if (voltageFieldIsEmpty) {
+                    _powerController.text =
+                        ((pow(current, 2)) * resistance).toString();
+                  } else if (resistanceFieldIsEmpty) {
+                    _powerController.text = (voltage * current).toString();
+                  } else {
+                    buildSnackbar(context, '  INPUT TWO FIELDS ');
+                  }
+                } else {
+                  buildSnackbar(context, '  INVALID INPUT / INPUTS ');
+                }
+              } catch (e) {
+                buildSnackbar(context, '  INVALID INPUT / INPUTS ');
+              }
+            })
           ],
         ),
       ),
@@ -143,9 +254,9 @@ class OhmsListTile extends StatelessWidget {
   const OhmsListTile({
     @required TextEditingController controller,
     @required this.dropDownMenuItems,
-  }) : _controller = controller;
+  }) : _controll = controller;
 
-  final TextEditingController _controller;
+  final TextEditingController _controll;
   final List<DropdownMenuItem<int>> dropDownMenuItems;
 
   @override
@@ -153,11 +264,12 @@ class OhmsListTile extends StatelessWidget {
     return ListTile(
       title: TextField(
         cursorWidth: 3.5,
-        controller: _controller,
+        keyboardType: TextInputType.numberWithOptions(),
+        controller: _controll,
         maxLength: 14,
         cursorColor: Color(0xff98DEDA),
         style: textStyling(fontSize: 30),
-        textAlign: TextAlign.center,
+        textAlign: TextAlign.start,
         decoration: InputDecoration(
           border: UnderlineInputBorder(),
           hintText: 'current',
@@ -167,9 +279,9 @@ class OhmsListTile extends StatelessWidget {
           input = input.toUpperCase();
           print(input);
         },
-        onEditingComplete: () {
-          _controller.clear();
-        },
+        // onEditingComplete: () {
+        //   _controll.clear();
+        // },
       ),
       trailing: DropdownButton(items: dropDownMenuItems, onChanged: null),
     );
